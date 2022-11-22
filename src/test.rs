075b363d9cc32d9b0bbf1fcca7148b5e3eda11b0
Tom Kunc <tomkunc0@gmail.com>
2022-11-22 14:14:14 +1100

    Split rs files, and add check and update_diff files.

---
+++ src/test.rs
@@ -0,0 +1,70 @@
+use imara_diff::intern::InternedInput;
+use imara_diff::{diff, Algorithm, UnifiedDiffBuilder};
+use std::io::{self, Write};
+use std::process::Command;
+use std::error::Error;
+
+pub fn test(exercise: String) -> Result<(), Box<dyn Error>> {
+    let color = if atty::is(atty::Stream::Stdout) {
+        "always"
+    } else {
+        "never"
+    };
+
+    let main_output = Command::new("cargo")
+        .arg("expand")
+        .arg("--color")
+        .arg(color)
+        .arg("--bin")
+        .arg(&exercise)
+        .arg("main")
+        .output()
+        .unwrap();
+
+    if !main_output.stderr.is_empty() {
+        println!("Got some errors when expand the macro:");
+        println!();
+        io::stderr().write_all(&main_output.stderr)?;
+    }
+
+    println!("This is the expansion you produced:");
+    println!();
+    io::stdout().write_all(&main_output.stdout)?;
+
+    let soln_output = Command::new("cargo")
+        .arg("expand")
+        .arg("--color")
+        .arg(color)
+        .arg("--bin")
+        .arg(format!("{exercise}_soln"))
+        .arg("main")
+        .output()
+        .unwrap();
+
+    println!();
+    println!("The expansion we expected is:");
+    println!();
+    io::stdout().write_all(&soln_output.stdout)?;
+
+    let before = String::from_utf8_lossy(&main_output.stdout);
+    let after = String::from_utf8_lossy(&soln_output.stdout);
+    let input = InternedInput::new(before.as_ref(), after.as_ref());
+    let the_diff = diff(
+        Algorithm::Histogram,
+        &input,
+        UnifiedDiffBuilder::new(&input),
+    );
+
+    if the_diff.is_empty() {
+        println!();
+        println!("Congratulation! You solved it.");
+        println!();
+    } else {
+        println!();
+        println!("The diff is:");
+        println!();
+        println!("{the_diff}");
+    }
+
+    Ok(())
+}
---
+++ src/update_diff.rs
@@ -0,0 +1,42 @@
+use imara_diff::intern::InternedInput;
+use imara_diff::{diff, Algorithm, UnifiedDiffBuilder};
+use std::path::PathBuf;
+use std::error::Error;
+
+use std::fs::File;
+use std::io::prelude::*;
+
+pub fn update_diff(exercise: &str) -> Result<(), Box<dyn Error>> {
+    let manifest_dir = env!("CARGO_MANIFEST_DIR");
+    let exercise_main_path: PathBuf = [manifest_dir, "exercises", exercise, "main.rs"]
+        .iter()
+        .collect();
+    let exercise_solution_path: PathBuf =
+        [manifest_dir, "exercises", exercise, "solutions", "main.rs"]
+            .iter()
+            .collect();
+    let exercise_diff_path: PathBuf = [
+        manifest_dir,
+        "exercises",
+        exercise,
+        "solutions",
+        "solution.diff",
+    ]
+    .iter()
+    .collect();
+
+    let exercise_main = std::fs::read_to_string(exercise_main_path)?;
+    let exercise_solution = std::fs::read_to_string(exercise_solution_path)?;
+
+    let input = InternedInput::new::<&str>(exercise_main.as_ref(), exercise_solution.as_ref());
+    let actual_diff = diff(
+        Algorithm::Histogram,
+        &input,
+        UnifiedDiffBuilder::new(&input),
+    );
+
+    let mut file = File::create(&exercise_diff_path)?;
+    file.write_all(actual_diff.as_bytes())?;
+
+    Ok(())
+}
