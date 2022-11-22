ca1b4a248d108250cf7562e0202f6576503f39cd
Tom Kunc <tomkunc0@gmail.com>
2022-11-22 14:18:34 +1100

    Push rustfmt

--- src/goal.rs
+++ src/goal.rs
@@ -1,5 +1,5 @@
-use std::process::{Command, Stdio};
 use std::error::Error;
+use std::process::{Command, Stdio};
 
 pub fn goal(exercise: String) -> Result<(), Box<dyn Error>> {
     Command::new("cargo")
