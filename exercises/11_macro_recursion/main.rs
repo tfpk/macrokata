3912d5b9ae11f9e8ab7a2aa40a09acda7e83fb8e
Tom Kunc <tomkunc0@gmail.com>
2022-12-23 13:54:11 +1100

    Push fix to 11_macro_recursion

--- exercises/11_macro_recursion/main.rs
+++ exercises/11_macro_recursion/main.rs
@@ -3,14 +3,19 @@
 ////////// DO NOT CHANGE BELOW HERE /////////
 
 fn print_numbers(nums: &Vec<i32>) {
-    println!("{nums:#?}");
+    println!("Resulting Numbers: {nums:#?}");
 }
 
 fn get_example_vec() -> Vec<i32> {
     vec![1, 3, 5, 6, 7, 9]
 }
 
+fn print_curried_argument(val: impl std::fmt::Debug) {
+    println!("Currying value {:?}.", val);
+}
+
 fn main() {
+    println!("=== defining functions ===");
     let is_between = curry!((min: i32) => (max: i32) => (item: &i32) => _, {
         min < *item && *item < max
     });
@@ -20,14 +25,18 @@ fn main() {
         vec.iter().filter_map(|i| if filter_between(i) { Some(*i) } else { None }).collect()
     });
 
+    println!("=== create between_3_7 ===");
     let between_3_7 = curry_filter_between(3)(7);
+    println!("=== create between_5_10 ===");
     let between_5_10 = curry_filter_between(5)(10);
 
     let my_vec = get_example_vec();
 
+    println!("=== call between_3_7 ===");
     let some_numbers: Vec<i32> = between_3_7(&my_vec);
     print_numbers(&some_numbers);
 
+    println!("=== call between_5_10 ===");
     let more_numbers: Vec<i32> = between_5_10(&my_vec);
     print_numbers(&more_numbers);
 }
--- exercises/11_macro_recursion/solutions/main.rs
+++ exercises/11_macro_recursion/solutions/main.rs
@@ -1,21 +1,29 @@
 macro_rules! curry {
     (_, $block:block) => {$block};
     (($argident:ident : $argtype:ty) => $(($argidents:ident: $argtypes:ty) =>)* _, $block:block) => {
-        move |$argident: $argtype| curry!($(($argidents: $argtypes) =>)* _, $block)
+        move |$argident: $argtype| {
+            print_curried_argument($argident);
+            curry!($(($argidents: $argtypes) =>)* _, $block)
+        }
     };
 }
 
 ////////// DO NOT CHANGE BELOW HERE /////////
 
 fn print_numbers(nums: &Vec<i32>) {
-    println!("{nums:#?}");
+    println!("Resulting Numbers: {nums:#?}");
 }
 
 fn get_example_vec() -> Vec<i32> {
     vec![1, 3, 5, 6, 7, 9]
 }
 
+fn print_curried_argument(val: impl std::fmt::Debug) {
+    println!("Currying value {:?}.", val);
+}
+
 fn main() {
+    println!("=== defining functions ===");
     let is_between = curry!((min: i32) => (max: i32) => (item: &i32) => _, {
         min < *item && *item < max
     });
@@ -25,14 +33,18 @@ fn main() {
         vec.iter().filter_map(|i| if filter_between(i) { Some(*i) } else { None }).collect()
     });
 
+    println!("=== create between_3_7 ===");
     let between_3_7 = curry_filter_between(3)(7);
+    println!("=== create between_5_10 ===");
     let between_5_10 = curry_filter_between(5)(10);
 
     let my_vec = get_example_vec();
 
+    println!("=== call between_3_7 ===");
     let some_numbers: Vec<i32> = between_3_7(&my_vec);
     print_numbers(&some_numbers);
 
+    println!("=== call between_5_10 ===");
     let more_numbers: Vec<i32> = between_5_10(&my_vec);
     print_numbers(&more_numbers);
 }
